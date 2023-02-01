31e879f5dec7ca05543e089cebf5ca5cac6d03ed
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-02-02 00:57:26 +0900

    incidentsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_01_105806) do
+ActiveRecord::Schema.define(version: 2023_02_01_154917) do
+
+  create_table "incidents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.integer "date", null: false
+    t.integer "place_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_incidents_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2023_02_01_105806) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "incidents", "users"
 end
