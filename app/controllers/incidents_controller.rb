fce78b47cea2d9f50754a6b29376a24c91bf9614
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-02-01 23:36:17 +0900

    新規登録/ログイン/ログアウト機能

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,4 +1,18 @@
 class IncidentsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
+  end
+
+  private
+
+  def set_incident
+    @incident = Incident.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
