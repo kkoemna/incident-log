f155462e1b1f42c3944c9f0618a7833bc440ba26
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-02-02 21:04:28 +0900

    インシデントモデルの単体テスト

--- spec/factories/incidents.rb
+++ spec/factories/incidents.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :incident do
-    
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.sentence }
+    date { Faker::Number.between(from: 20000101, to: 20991231) }
+    place_id { Faker::Number.between(from: 1, to: 24) }
+    association :user
+
+    after(:build) do |incident|
+      incident.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
