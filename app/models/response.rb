25f4ee17b33d6612cf84ca7608cf445ca365f9b1
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-02-10 02:14:44 +0900

    newアクションの作成

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,5 @@
 class Response < ApplicationRecord
   belongs_to :user
   belongs_to :incident
+  has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'incidents#index'
   resources :incidents
+  resources :responses
 end
