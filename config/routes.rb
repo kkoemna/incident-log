5c73e0a44b7e17a43b192ae992a54e50c07ab408
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-02-13 23:35:36 +0900

    createアクションの定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'incidents#index'
   resources :incidents do
     resources :responses, only: [:new, :create, :edit, :update, :destroy]
+    resources :comments,  only: :create
   end
 end
