a5619a10bc84a7a55bbb1e548c07a557dc0bcf56
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-02-15 07:34:24 +0900

    バリデーションの設定

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,8 +1,8 @@
 class Incident < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :date,     presence: true
   validates :title,    presence: true
   validates :content,  presence: true
-  validates :date,     presence: true
   validates :place_id, numericality: { other_than: 0 , message: "can't be blank" }
   belongs_to :user
   belongs_to :place
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,8 @@
 class Response < ApplicationRecord
+  validates :date,    presence: true
+  validates :title,   presence: true
+  validates :content, presence: true
+  validates :cost,    presence: true
   belongs_to :user
   belongs_to :incident
   has_one_attached :image
