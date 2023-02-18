0878fedf0b8b556772fb4cd5fd61b1c4f5fac313
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-02-18 12:33:46 +0900

    デプロイ正常化のための修正（5）

--- babel.config.js
+++ babel.config.js
@@ -37,11 +37,16 @@ module.exports = function(api) {
       ]
     ].filter(Boolean),
     plugins: [
-      ["@babel/plugin-proposal-private-methods", { "loose": true }]
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
+      [
+        '@babel/plugin-proposal-private-methods',
+       {
+         loose: true 
+        }
+      ],
       [
         '@babel/plugin-proposal-class-properties',
         {
